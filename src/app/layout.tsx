44b020869ba1a238ceeed90a6ebe8ac16bef87d9
Khan.Tran <khan.tran@ptnglobalcorp.com>
2023-06-27 10:54:52 +0700

    feat: add @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import { AppHeader } from "@/components/layout/AppHeader";
 import { AppFooter } from "@/components/layout/AppFooter";
+import { Analytics } from "@vercel/analytics/react";
 
 const roboto = Roboto({
   weight: "400",
@@ -25,6 +26,7 @@ export default function RootLayout({
         <AppHeader />
         {children}
         <AppFooter />
+        <Analytics />
       </body>
     </html>
   );
